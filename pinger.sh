d03132ff79f02a734adb2249cb00f53117d44666
Azouz <aziz50607080@gmail.com>
2017-04-27 22:44:17 +0100

    Pinger

---
+++ pinger.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+------- ps aux | grep -m 1 elasticsearch | awk '{print $2}' ------- prints the pid of process
+if [ $# != 1  ]; then
+	echo "Pinger needs One argument, exactly!"
+else
+	ping -c 1 $1 &>/dev/null
+        if [ "${?}" -eq 68 ]; then
+            echo "Pinger couldnt resolve the Adress/Url, Sorry"
+        else
+            ping -c 1 $1 | grep -o -m 1 '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+        fi
+fi
