c5b11bf5c5c085cc63c8330d52e0cdb4ee6d9569
Azouz <aziz50607080@gmail.com>
2017-06-01 13:20:07 +0100

    Remove tmp file file created as a timestamp reference

--- todaysFilesGetter.sh
+++ todaysFilesGetter.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /Users/azouz/Documents/
-touch -t `date +%m%d0000` $(date +"%Y-%m-%d")-files
-find /Users/azouz/ -size +50k -type f -newer $(date +"%Y-%m-%d")-files > /Users/azouz/Documents/$(date +"%Y-%m-%d")_files_list
+touch -t `date +%m%d0000` file-date-reference
+find /Users/azouz/ -size +50k -type f -newer file-date-reference > /Users/azouz/Documents/$(date +"%Y-%m-%d")_files_list
+rm file-date-reference
